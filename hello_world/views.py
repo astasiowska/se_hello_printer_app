93df749e3ba4aeceedafcb31081462e1e76095db
agnieszkas <agnieszkas@users.noreply.github.com>
2020-05-24 00:32:35 +0200

    Modyfikacje dla XML

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Aga"
+moje_imie = "Agnieszka"
 msg = "Hello World!"
 
 
--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,8 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Aga", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Agnieszka", "mgs":Hello World!"}', rv.data)
+
+    def test_xml(self):
+        rv = self.app.get('/?output=xml')
+        self.assertIn(b"<greetings>", rv.data)
